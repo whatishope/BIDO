9421f0648a0efa9b4eccf0a5758338183d2f767d
whatishope <165648166+whatishope@users.noreply.github.com>
2025-08-25 15:34:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ GPU = '0'
 workers = 4                 
 epochs = 50                
 batch_size = 8
-learning_rate = 1e-3        # 学习率
-num_attn_layers=2       #注意力层数
+learning_rate = 1e-3        
+num_attn_layers=2     
 drop_rate=0.1
 n_cluster=10
 num_classes=2
@@ -14,21 +14,21 @@ D_xml=512
 ##################################################
 # Model Config
 ##################################################
-image_size = (448, 448)     # 调整图像大小
-net = 'inception_mixed_6e'  # feature extractor
-num_attentions = 32         # 每一张图对应的局部特征区间
-beta = 5e-2                 # 特征重心
+image_size = (448, 448)     
+net = 'inception_mixed_6e'  
+num_attentions = 32         
+beta = 5e-2                 
 ##################################################
 # Dataset/Path Config
 ##################################################
 tag = 'bird'             
 
-#存储.ckpt文件位置
+
 save_dir = './IIMD/'
 model_name = 'best_model.pth'
 log_name = 'train.log'
 
-# checkpoint model for resume training
+
 ckpt = False
 # ckpt = save_dir + model_name
 
